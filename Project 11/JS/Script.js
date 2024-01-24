dfd99df305d4f2dd296333701b27eff7349a54cb
Abhishek Kumar <developera@localhost.localdomain>
2024-01-24 06:22:02 +0530

    inprove bad code

--- Project 11/JS/Script.js
+++ Project 11/JS/Script.js
@@ -1,7 +1,13 @@
-const apikey = "appid=270e2e22c4af70cffb18e552660865ad";
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&';
+const apikey = "701363f28fde43d0aa4165056242301";
+// const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&';
 
+const apiUrl = `http://api.weatherapi.com/v1/current.json?key=` ;
 // console.log(apiUrl + apikey)
+// ${(apikey)}&q=Bagodar&aqi=yes
+
+const WeatherIcon = document.getElementById('weather-icon');
+
+
 
 
 const searchBox = document.getElementById('input');
@@ -12,7 +18,7 @@ const button =  document.addEventListener('click', function(){
 })
 
 const buttonenter =  document.addEventListener('keypress', function(keypress){
-console.log(keypress)
+// console.log(keypress) 
 const weatherData =  searchBox.value;
 if(keypress.key === 'Enter'){
 
@@ -26,14 +32,36 @@ if(keypress.key === 'Enter'){
 async function checkWeater(dets){
 
 
-    const response = await fetch(apiUrl + apikey + `&q=${dets}` );
+    const response = await fetch(apiUrl + apikey + `&q=${dets}` + '&aqi=yes') ;
+    // http://api.weatherapi.com/v1/current.json?key=a701363f28fde43d0aa4165056242301&q=bagodar&aqi=yes
+console.log(response)
     var data = await response.json();
-    console.log(data)
-   
-    document.querySelector('.city').innerHTML = data.name;
-    document.querySelector('.temp').innerHTML = Math.round(data.main.temp)+ '°C';
-    document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
-    document.querySelector('.wind').innerHTML = data.wind.speed + 'Km/h';
+    // console.log(data)
+
+    document.querySelector('.city').innerHTML = data.location.name;
+    document.querySelector('.temp').innerHTML = Math.round(data.current.temp_c)+ '°C';
+    document.querySelector('.humidity').innerHTML = data.current.humidity + '%';
+    document.querySelector('.wind').innerHTML = data.current.wind_kph + 'Km/h';
+
+    if(data.current.condition === "Clouds")  {
+WeatherIcon.src = '../Image/clouds.png'
+}else if(data.current.condition.text === "Clear"){
+    WeatherIcon.src = '../Image/clear.png'
+}else if(data.current.condition.text === "Mist"){
+    WeatherIcon.src = '../Image/mist.png'
+}else if(data.current.condition === "Partly cloudy"){
+    WeatherIcon.src = '../Image/clouds.png'
+}else if(data.current.condition.text === "Freezing fog"){
+    WeatherIcon.src = '../Image/snow.png'
+}
+else if(data.current.condition.text === "Light rain"){
+    WeatherIcon.src = '../Image/rain.png'
+}
+else {
+    WeatherIcon.src = '../Image/drizzle.png'
+}
+
+
 
 }
 
